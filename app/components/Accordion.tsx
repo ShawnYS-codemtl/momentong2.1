42467427ace518e1de8809bac7531bd1c4d4b8f5
ShawnYS-codemtl <syatsin@gmail.com>
2026-01-15 03:41:12 -0500

    feat: add Accordion component for collapsible content; update StickerDetailPage to include Accordion for description and additional information, and enhance styles for sticker detail layout

---
+++ app/components/Accordion.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useState } from "react"
+
+interface AccordionProps {
+    title: string
+    children: React.ReactNode
+}
+
+export default function Accordion({title, children}: AccordionProps) {
+    const [open, setOpen] = useState(false)
+
+    return (
+        <div className="w-full">
+          <button
+            className="flex items-center justify-between w-full py-5 cursor-pointer"
+            onClick={() => setOpen(!open)}
+            aria-expanded={open}
+          >
+            <span className="font-medium">{title}</span>
+            <span>{open ? '−' : '+'}</span>
+          </button>
+    
+          {open && (
+            <div className="pb-5">
+              {children}
+            </div>
+          )}
+        </div>
+      )      
+}
+
--- app/stickers/[slug]/page.tsx
+++ app/stickers/[slug]/page.tsx
@@ -1,6 +1,9 @@
+
 import { getStickerBySlug, getStickerUrl } from "@/lib/data/stickers"
-
-
+import Image from "next/image"
+import { formatSlug } from "@/lib/utils/formatSlug"
+import Accordion from "@/app/components/Accordion"
+import Breadcrumb from "@/app/components/Breadcrumb"
 
 interface Params {
     params: { slug: string }
@@ -11,16 +14,55 @@ export default async function StickerDetailPage({params} : Params){
     const sticker = await getStickerBySlug(slug)
 
     return (
-        <main className="px-8 py-12">
-          <h2 className="text-2xl font-bold mb-6">{sticker.title}</h2>
-          <img
-            src={getStickerUrl(sticker.image_path)}
-            alt={sticker.title}
-            width={sticker.width}
-            height={sticker.height}
-          />
-          <p className="mt-4">{sticker.description}</p>
-          <p className="mt-2 font-bold">${sticker.price.toFixed(2)} CAD</p>
+        <main className="px-[100px]">
+            <Breadcrumb 
+                items={[
+                    {label: 'Home', href: '/'},
+                    {label: 'Stickers', href: '/stickers'},
+                    {label: formatSlug(slug)}
+                ]}
+            />
+            <div className="flex my-8">
+                <div className="sticker-detail-left">
+                    <Image
+                        className="sticker-detail-img"
+                        src={getStickerUrl(sticker.image_path)}
+                        alt={sticker.title}
+                        width={sticker.width}
+                        height={sticker.height}
+                        >
+                    </Image>
+                </div>
+                <div className="sticker-detail-info px-8">
+                    <h2 className="mb-3">{sticker.title.toUpperCase()}</h2>
+                    <h3 className="mb-4">$ {sticker.price.toFixed(2)} CAD</h3>
+                    <hr />
+                    <ul className="my-5 flex flex-col gap-[10px] ">
+                        <li><p>💧 Dishwasher safe</p></li>
+                        <li><p>☂️ Weatherproof</p></li>
+                        <li><p>⭐️ Fade resistant</p></li>
+                    </ul>
+                    <p>Quantity</p>
+                    <div className="quantity-button mt-3">
+                        <button>-</button>
+                        <span> 0 </span>
+                        <button>+</button>
+                    </div>
+                    <button className="add-to-cart-btn my-9">ADD TO CART  - ${sticker.price.toFixed(2)}</button>
+                    <hr />
+                    <Accordion title="Description">
+                        <p>{sticker.description}</p>
+                    </Accordion>
+                    <hr />
+                    <Accordion title="Additional Information">
+                       <p>Size:...</p>
+                       <p>Shipping:...</p>
+                       <p>Care Instructions:</p>
+                       <p> Returns:</p>   
+                    </Accordion>
+                </div>
+
+            </div>
         </main>
       )
 }
