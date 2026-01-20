66b3c6541b11dfb308c74c618716fb15b6064731
ShawnYS-codemtl <syatsin@gmail.com>
2026-01-20 18:45:04 -0500

    style: adjust sticker positioning and sizing in header

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -9,7 +9,7 @@ export default function Hero () {
         <div className="hero-container">
             <h1>Because some <span className="accent stroke-word">moments</span> deserve to <span className="accent">stick</span>
             </h1>
-            <h2>Capturing life’s fleeting magic in lovingly hand-drawn stickers</h2>
+            <h2 className="text-center">Capturing life’s fleeting magic in lovingly hand-drawn stickers</h2>
             <ul className="hero-stickers">
                 <li><Image src={FilmCam} alt="film cam sticker" className="hero-sticker hero-sticker-filmcam" width={300} height={236}></Image></li>
                 <li><Image src={HKStampDay} alt="HK stamp daytime sticker" className="hero-sticker hero-sticker-stamp" width={300} height={412}></Image></li>
